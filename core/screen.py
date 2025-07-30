b468cec53e3b2885c81274da8eefd1c8438f3501
Nguyễn Trung Hiếu <shimakazevn@gmail.com>
2025-07-31 04:53:05 +0700

    Refactor screen size adjustment to use win32con constants for better clarity

--- core/screen.py
+++ core/screen.py
@@ -1,5 +1,6 @@
 import ctypes
 import win32api
+import win32con
 
 def get_screen_size():
     return (
@@ -11,6 +12,5 @@ def set_screen_size(width, height):
     devmode = win32api.EnumDisplaySettings(None, 0)
     devmode.PelsWidth = width
     devmode.PelsHeight = height
-    devmode.BitsPerPel = 32
-    devmode.DisplayFixedOutput = 0
+    devmode.Fields = win32con.DM_PELSWIDTH | win32con.DM_PELSHEIGHT
     win32api.ChangeDisplaySettings(devmode, 0) 
